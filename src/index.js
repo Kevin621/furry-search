d8b538dcb7d4b6ee05005a4ecb87cfd1e29a9c88
Kevin621 <60020468+Kevin621@users.noreply.github.com>
2020-09-14 21:50:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const snekfetch = require('snekfetch');
 /**
  * 
  * @param {string} url 
- * @returns {string}
+ * @returns {array}
  */
-function search(url) {
+async function search(url) {
     try {
         const { body } = await snekfetch
             .get(`https://saucenao.com/search.php?db=999&output_type=2&testmode=1&numres=16&url=${url}`)
@@ -13,15 +13,16 @@ function search(url) {
         if(simi < 80){
             console.log('failed to find result')
         }
-        let simil = Number(body.results[0].header.similarity);
-        let link = body.results[0].data.source;
-        let artist = body.results[0].data.creator;
+        const simil = Number(body.results[0].header.similarity);
+        const link = body.results[0].data.source;
+        const artist = body.results[0].data.creator;
         
-        return [simil, link, artist]
+        return console.log(simil, link, artist)
     } catch(err) {
         return console.log(err);
     }
 };
 module.exports = search;
+
 
 console.log(search("https://cdn.discordapp.com/attachments/730477809675534349/754894623515607080/IMG_20200912_192433_986.jpg"))
