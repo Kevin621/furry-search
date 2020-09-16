57546ad971d1d9dc4069c9ec0de9db0fd2a77327
Kevin621 <60020468+Kevin621@users.noreply.github.com>
2020-09-15 22:11:50 -0400

    Update Patch

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+
+/**
+ * 
+ * @param {string} url 
+ */
+async function search(url) {
+    const fetch = require('node-fetch');
+    new fetch
+        (`https://saucenao.com/search.php?db=999&output_type=2&testmode=1&numres=16&url=${url}`)
+    .then(res => res.json())
+    .then(json => {
+        
+        const source = json.results[0].data.source;
+        const artist = json.results[0].data.creator;
+        const ext_urls = json.results[0].data.ext_urls;
+
+        return console.log(`Source: ${source}
+Artist: ${artist}
+ext_urls: ${ext_urls}`)
+    })
+
+}
+module.exports = search; 
+
+
+
+
+
